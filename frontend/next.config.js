2a80eddd643d2180ff5bd3bec220988d1903a9a0
Arpit Mohapatra <arpitm@mac.home>
2025-04-16 14:10:46 -0400

    updated next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/ai-generated-resume" : "",
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "/ai-generated-resume/" : "",
   images: {
     unoptimized: true,
   },
